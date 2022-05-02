3280a3eae211fe79f645cbf39637a7a4d6a4c0af
mohnishs1975 <mohnishs@bu.edu>
2022-05-02 17:21:14 -0400

    updates

--- frontend/src/components/Welcome.js
+++ frontend/src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
         <div class="container-fluid bg-light text-dark p-5">
             <div class="container bg-light p-5">
                 <h1 class="display-4 fw-bold">Images Gallery</h1>
-                    <p>This is a simple application that searches for free high quality photos. To start enter a search term in the search field.</p>
+                    <p>This is a simple application that searches for free high quality photos. To start enter a term in the search field.</p>
                     <a href="https://unsplash.com" class="btn btn-primary">Learn More</a>
             </div>
         </div>
