a4a1d3093cb45c5778f2a4939910f029a6216b42
mohnishs1975 <mohnishs@bu.edu>
2022-05-02 20:01:04 -0400

    integrate Flask/enable cors

--- frontend/api/app.py
+++ frontend/api/app.py
@@ -1,7 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import requests
+import os
+from dotenv import load_dotenv
+from flask_cors import CORS
+
+load_dotenv(dotenv_path="./.env.local")
+
+UNSPLASH_URL='https://api.unsplash.com/photos/random'
+UNSPLASH_KEY=os.environ.get("UNSPLASH_KEY", "")
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello():
-    return "Hello, World!"
+@app.route("/new-image")
+def new_image():
+    word = request.args.get("query")
+    headers = {
+        "Authorization": "Client-ID " + UNSPLASH_KEY,
+        "Accept-Version": "v1"
+
+    }
+    params = {
+        "query": word
+    }
+    response = requests.get(url=UNSPLASH_URL,headers=headers, params=params)
+    data = response.json()
+    return data
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Welcome from './components/Welcome'
 
 
-const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
+const API_URL = process.env.REACT_APP_API_URL  || 'http://127.0.0.1:5000';
 
 const App = () => {
   const [word, setWord] = useState("")
@@ -15,7 +15,7 @@ const App = () => {
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
-    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+    fetch(`${API_URL}/new-image?query=${word}`)
       .then((res) => res.json())
       .then((data) => {
         setImages([{ ...data, title: word }, ...images]) //... operator means adding to existing array of images
