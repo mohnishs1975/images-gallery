f76ee807de5b877a07e0da807178a831fcda1784
mohnishs1975 <mohnishs@bu.edu>
2022-05-08 17:37:58 -0400

    volume mapping added

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -17,7 +17,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
                             </Col>
                             <Col>
                                 <Button variant="primary" type="submit">
-                                    Search
+                                    Search 
                                 </Button>
                             </Col>
                         </Row>
